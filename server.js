1cef8e499a203626e90c9f9b3d3bfcbb1183b1b1
Pipat Angkaew <pipiat0909737525@gmail.com>
2024-08-15 22:30:27 +0700

    add: readme

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const app = require("./app");
 
-const PORT = process.env.PORT || 6000;
+const PORT = process.env.PORT || 8282;
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
